be4c716896e06a989f8f7757fac2260f235845a6
pKallert <paula.kallert@paessler.com>
2025-03-20 13:02:49 +0100

    Fix: Undo changes to sync dialogue

--- packages/neos-ui-sagas/src/Sync/index.ts
+++ packages/neos-ui-sagas/src/Sync/index.ts
@@ -124,6 +124,8 @@ export const makeResolveConflicts = (deps: {
                         yield * discardAll();
                         return false; // don't continue publishing as this is a deletes all
                     }
+
+                    continue;
                 }
             }
 
