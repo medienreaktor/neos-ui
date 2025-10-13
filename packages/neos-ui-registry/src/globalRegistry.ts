e235199ee36cbf3d6abbd702e226b3d8e34d5954
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-10-13 20:11:43 +0200

    TASK: Solve `SynchronousMetaRegistry` vs `GlobalRegistry` type riddle

--- packages/neos-ui-registry/src/globalRegistry.ts
+++ packages/neos-ui-registry/src/globalRegistry.ts
@@ -1,8 +1,6 @@
 import {SynchronousMetaRegistry, SynchronousRegistry} from './registry';
 
-export interface GlobalRegistry {
-    get<T>(key: string): SynchronousRegistry<T> | null;
-    set<T>(key: string, registry: SynchronousRegistry<T>): SynchronousRegistry<T>;
+export interface GlobalRegistry extends SynchronousMetaRegistry<SynchronousRegistry<unknown>> {
 }
 
 /**
@@ -11,8 +9,7 @@ export interface GlobalRegistry {
  * FIXME
  * Note that we often pass the global registry around instead and inject it via the react context -> this should be simplified in favour of this global state.
  */
-// FIXME SynchronousMetaRegistry vs GlobalRegistry type dilemma
-const globalRegistry = new SynchronousMetaRegistry(`The global registry`) as unknown as GlobalRegistry;
+const globalRegistry = new SynchronousMetaRegistry(`The global registry`) as GlobalRegistry;
 
 export const getRegistryById: GlobalRegistry['get'] = (key) => {
     // @ts-ignore
