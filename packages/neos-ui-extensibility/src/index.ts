74faaf198d7da214bac4a22b6dffad5a4c8bc9ad
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2025-10-13 18:59:22 +0200

    TASK: Add correct typescript typing to `manifest()` function

--- packages/neos-ui-extensibility/src/index.ts
+++ packages/neos-ui-extensibility/src/index.ts
@@ -1,7 +1,27 @@
 import createConsumerApi from './createConsumerApi';
 import readFromConsumerApi from './readFromConsumerApi';
 
-export default readFromConsumerApi('manifest');
+type BootstrapDependencies = {
+    // correct store type depends on https://github.com/neos/neos-ui/pull/4010
+    store: any;
+    frontendConfiguration: Record<string, unknown>;
+    configuration: Record<string, unknown>;
+    routes: Record<string, unknown>;
+};
+
+type Bootstrap = (
+    globalRegistry: any,
+    bootstrapDependencies: BootstrapDependencies
+) => void;
+
+const manifest: (
+    identifier: string,
+    // the age-old commit a2dae5b78a649ad75643f48b02b6df67f5915a58 introduced this parameter "options" though we not make use of it
+    options: {},
+    bootstrap: Bootstrap
+) => void = readFromConsumerApi('manifest');
+
+export default manifest;
 
 export {
     createConsumerApi,
