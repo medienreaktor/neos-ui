7ad0a395542e50b5bc0d0ae9ffb5784a2174ca72
Sebastian Helzle <sebastian@helzle.it>
2025-11-04 12:05:34 +0100

    BUGFIX: Show new ckeditor toolbar in RichTextEditor

---
+++ packages/neos-ui-editors/src/SecondaryEditors/CKEditorWrap/index.tsx
@@ -0,0 +1,114 @@
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import DecoupledEditor from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor';
+import debounce from 'lodash.debounce';
+
+import {neos} from '@neos-project/neos-ui-decorators';
+import {EditorToolbar} from '@neos-project/neos-ui-ckeditor5-bindings/src/EditorToolbar';
+import {useSelector} from '@neos-project/neos-ui-redux-store';
+import {GlobalRegistry} from '@neos-project/neos-ui-registry';
+import {CkEditorConfigRegistry} from '@neos-project/neos-ui-ckeditor5-bindings/src/registry/CkEditorConfigRegistry';
+
+import style from './index.module.css';
+
+const withNeosGlobals = neos((globalRegistry) => ({
+    globalRegistry,
+    configRegistry: globalRegistry.get('ckEditor5').get('config'),
+}));
+
+type CKEditorWrapProps = {
+    onChange: (value: string) => void,
+    value: string,
+    options: object,
+    configRegistry: CkEditorConfigRegistry,
+    globalRegistry: GlobalRegistry
+}
+
+const CKEditorWrap: React.FC<CKEditorWrapProps> = ({
+                                                       onChange,
+                                                       value,
+                                                       options,
+                                                       configRegistry,
+                                                       globalRegistry
+                                                   }) => {
+    const userPreferences = useSelector(state => state?.user?.preferences);
+    const [formattingUnderCursor, setFormattingUnderCursor] = useState({});
+    const [lastFormattingUnderCursorSerialized, setLastFormattingUnderCursorSerialized] = useState('');
+    const editorRef = useRef<HTMLElement>();
+    const toolbarWrapRef = useRef<HTMLElement>();
+    const [currentEditor, setCurrentEditor] = useState<DecoupledEditor>();
+
+    const executeCommand = useCallback((command, argument, reFocusEditor = true) => {
+        if (!currentEditor) {
+            return;
+        }
+        currentEditor.execute(command, argument);
+        if (reFocusEditor) {
+            currentEditor.editing.view.focus();
+        }
+    }, [currentEditor]);
+
+    const handleUserInteractionCallback = useCallback(() => {
+        if (!currentEditor) {
+            return;
+        }
+        const newFormattingUnderCursor: Record<string, any> = {};
+        [...currentEditor.commands].forEach(commandTuple => {
+            const [commandName, command] = commandTuple;
+            if (command.value !== undefined) {
+                newFormattingUnderCursor[commandName] = command.value;
+            }
+        });
+
+        const formattingUnderCursorSerialized = JSON.stringify(newFormattingUnderCursor);
+        if (formattingUnderCursorSerialized !== lastFormattingUnderCursorSerialized) {
+            setFormattingUnderCursor(newFormattingUnderCursor);
+            setLastFormattingUnderCursorSerialized(formattingUnderCursorSerialized);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!editorRef.current) {
+            return;
+        }
+        const domNode = editorRef.current;
+
+        const ckeConfig = configRegistry.getCkeditorConfig({
+            editorOptions: options,
+            userPreferences,
+            globalRegistry,
+            propertyDomNode: domNode
+        });
+
+        DecoupledEditor
+            .create(domNode, {
+                ...ckeConfig,
+                initialData: value
+            })
+            .then(editor => {
+                setCurrentEditor(editor);
+
+                // As we use the DecoupledEditor, we need to add the toolbar to the secondary editor
+                if (toolbarWrapRef.current) {
+                    toolbarWrapRef.current.appendChild(editor.ui.view.toolbar.element);
+                }
+
+                editor.model.document.on('change', handleUserInteractionCallback);
+                editor.model.document.on('change:data', debounce(() => onChange(editor.getData()), 200, {maxWait: 3000}));
+            }).catch(e => console.error(e));
+    }, [editorRef.current]);
+
+    return (
+        <div className={style.wrap}>
+            <div className={style.toolBar__wrap} ref={toolbarWrapRef}>
+                <EditorToolbar
+                    executeCommand={executeCommand}
+                    editorOptions={options}
+                    formattingUnderCursor={formattingUnderCursor}
+                />
+            </div>
+            <div ref={editorRef} className={style.editor}/>
+        </div>
+    );
+}
+
+export default React.memo(withNeosGlobals(CKEditorWrap as any));
