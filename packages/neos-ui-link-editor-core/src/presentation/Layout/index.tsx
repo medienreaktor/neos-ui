cc019321d27172f0e067bee6384c6bb0415303ce
Sebastian Helzle <sebastian@helzle.it>
2025-11-18 15:48:57 +0100

    TASK: Remove unused container component

--- packages/neos-ui-link-editor-core/src/presentation/Layout/index.tsx
+++ packages/neos-ui-link-editor-core/src/presentation/Layout/index.tsx
@@ -1,6 +1,4 @@
 import style from './style.module.css';
 import React from 'react';
 
-export const Container: React.FC = ({children}) => <div className={style.elipsis}>{children}</div>;
-
 export const Stack: React.FC = ({children}) => <div className={style.stack}>{children}</div>;
