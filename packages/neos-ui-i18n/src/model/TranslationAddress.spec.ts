fe3d4c5f6d97d30f1c7b6836beef742b2ce0a936
Robin Roloff <116283647+robinroloff@users.noreply.github.com>
2026-01-22 10:35:27 +0100

    BUGFIX: Refactor `tryFromString`  (#4070)
    * Refactor `tryFromString` to improve parsing logic and handle cases with extra delimiters.
    
    * Validate input in `tryFromString` to ensure minimum required parts.
    
    * Linting
    
    * Added tests for new behaviour in TranslationAddress creation
    
    * linting
    
    * task: new test to assert that empty segments are invalid
    
    * task: new test to assert that empty segments are invalid

--- packages/neos-ui-i18n/src/model/TranslationAddress.spec.ts
+++ packages/neos-ui-i18n/src/model/TranslationAddress.spec.ts
@@ -34,6 +34,17 @@ describe('TranslationAddress', () => {
         expect(translationAddress.fullyQualified).toBe('Some.Package:SomeSource:some.transunit.id');
     });
 
+    it('can be created from string with more than one colon', () => {
+        const translationAddress = TranslationAddress.fromString(
+            'Some.Package:SomeSource:some.transunit.id:extra'
+        );
+
+        expect(translationAddress.id).toBe('some.transunit.id:extra');
+        expect(translationAddress.sourceName).toBe('SomeSource');
+        expect(translationAddress.packageKey).toBe('Some.Package');
+        expect(translationAddress.fullyQualified).toBe('Some.Package:SomeSource:some.transunit.id:extra');
+    });
+
     it('throws if given an invalid string', () => {
         expect(() => TranslationAddress.fromString('foo bar'))
             .toThrow(
@@ -54,10 +65,33 @@ describe('TranslationAddress', () => {
         expect(translationAddress?.fullyQualified).toBe('Some.Package:SomeSource:some.transunit.id');
     });
 
+    it('can be trycreated from string with more than one colon', () => {
+        const translationAddress = TranslationAddress.tryFromString(
+            'Some.Package:SomeSource:some.transunit.id:extra'
+        );
+
+        expect(translationAddress).not.toBeNull();
+        expect(translationAddress?.id).toBe('some.transunit.id:extra');
+        expect(translationAddress?.sourceName).toBe('SomeSource');
+        expect(translationAddress?.packageKey).toBe('Some.Package');
+        expect(translationAddress?.fullyQualified).toBe('Some.Package:SomeSource:some.transunit.id:extra');
+    });
+
     it('try with invalid string returns null', () => {
         expect(TranslationAddress.tryFromString('foo bar')).toBeNull();
         expect(TranslationAddress.tryFromString('something:')).toBeNull();
         // error in placeholder https://github.com/neos/neos-ui/pull/3907
         expect(TranslationAddress.tryFromString('ClientEval: node.properties.tagName')).toBeNull();
+    });
+
+    it('try with empty segments returns null', () => {
+        expect(TranslationAddress.tryFromString('::')).toBeNull();
+        expect(TranslationAddress.tryFromString('a::')).toBeNull();
+        expect(TranslationAddress.tryFromString(':a:')).toBeNull();
+        expect(TranslationAddress.tryFromString(':a')).toBeNull();
+        expect(TranslationAddress.tryFromString('::a')).toBeNull();
+
+        expect(TranslationAddress.tryFromString('a:b:')).toBeNull();
+        expect(TranslationAddress.tryFromString('a::b')).toBeNull();
     });
 });
