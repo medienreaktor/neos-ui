7c3c63657f57f9ce38bfa73cc0104f04ff9fb6c2
Michael <michael.berhorst@sandstorm.de>
2026-03-31 13:27:16 +0200

    TASK: Tests for preview mode badge

--- packages/jest-preset-neos-ui/src/setupBrowserEnv.js
+++ packages/jest-preset-neos-ui/src/setupBrowserEnv.js
@@ -3,3 +3,30 @@ import browserEnv from 'browser-env';
 import 'cross-fetch/polyfill';
 
 browserEnv();
+
+// Provide a minimal #initialData element so that @neos-project/neos-ui-configuration
+// can initialise without crashing during test module load.
+// jsdom (browser-env) does not implement innerText, so we define it directly on
+// the element instance as getInlinedDataFromBackend reads initialDataContainer.innerText.
+const initialDataContent = JSON.stringify({
+    configuration: {
+        nodeTree: {},
+        structureTree: {},
+        editPreviewModes: {
+            inPlace: {isEditingMode: true, isPreviewMode: false, title: 'In-Place'},
+            previewMode: {isEditingMode: false, isPreviewMode: true, title: 'Preview Mode'}
+        }
+    },
+    frontendConfiguration: {}
+});
+const initialDataEl = document.createElement('script');
+initialDataEl.id = 'initialData';
+initialDataEl.type = 'application/json';
+initialDataEl.textContent = initialDataContent;
+Object.defineProperty(initialDataEl, 'innerText', {
+    get() {
+        return this.textContent;
+    },
+    configurable: true
+});
+document.head.appendChild(initialDataEl);
--- packages/neos-ui/src/Containers/SecondaryToolbar/PreviewBadge/index.spec.js
+++ packages/neos-ui/src/Containers/SecondaryToolbar/PreviewBadge/index.spec.js
@@ -3,20 +3,13 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import {mount} from 'enzyme';
 import {setupI18n} from '@neos-project/neos-ui-i18n';
-import {getConfiguration} from '@neos-project/neos-ui-configuration';
 import PreviewBadge from './index';
-
-jest.mock('@neos-project/neos-ui-configuration', () => ({
-    getConfiguration: jest.fn()
-}));
 
 beforeAll(() => {
     setupI18n('en-US', 'one,other', {});
 });
 
 test('PreviewBadge > is visible in preview mode', () => {
-    getConfiguration.mockReturnValue({previewMode: {isPreviewMode: true}});
-
     const store = createStore(state => state, {
         ui: {editPreviewMode: 'previewMode'}
     });
@@ -31,8 +24,6 @@ test('PreviewBadge > is visible in preview mode', () => {
 });
 
 test('PreviewBadge > is hidden in edit mode', () => {
-    getConfiguration.mockReturnValue({inPlace: {isPreviewMode: false}});
-
     const store = createStore(state => state, {
         ui: {editPreviewMode: 'inPlace'}
     });
